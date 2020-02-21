c48123c25e17209ef898df297b6bce683b0b4fb3
sadikuar <artan.sadiku@he-arc.ch>
2020-02-21 09:34:51 +0100

    Finished security project

--- ProjetClasseEtudiant/src/main/java/com/example/classeetudian/WebSecurityConfig.java
+++ ProjetClasseEtudiant/src/main/java/com/example/classeetudian/WebSecurityConfig.java
@@ -1,46 +1,45 @@
 package com.example.classeetudian;
 
+import javax.sql.DataSource;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.DelegatingPasswordEncoder;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
+@EnableAutoConfiguration
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-//		http.authorizeRequests().antMatchers("/", "/home").permitAll()
-//				// .anyRequest().authenticated()
-//				// .anyRequest().hasRole("USER")
-//				.antMatchers("/admin").hasRole("ADMIN").antMatchers("/user").hasRole("USER").antMatchers("/hello")
-//				.hasAnyRole("USER", "ADMIN").and().formLogin().loginPage("/login").permitAll().and().logout()
-//				.permitAll();
-		http.authorizeRequests()
-			.antMatchers("/", "/home")
-			.permitAll()
-			.antMatchers("/formEtudiant")
-			.hasAnyRole("USER", "ADMIN")
-			.antMatchers("/insertEtudiant")
-			.hasAnyRole("USER", "ADMIN")
-			.antMatchers("/user")
-			.hasAnyRole("USER", "ADMIN")
-			.antMatchers("/admin")
-			.hasRole("ADMIN")
-			.and()
-			.formLogin()
-			.loginPage("/login")
-			.permitAll()
-			.and()
-			.logout().permitAll();
-	}
 
 	@Autowired
-	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-		auth.inMemoryAuthentication().withUser("admin").password("{noop}admin").roles("ADMIN").and().withUser("user")
-				.password("{noop}user").roles("USER");
+	DataSource dataSource;
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests().antMatchers("/", "/home").permitAll().antMatchers("/formEtudiant")
+				.hasAnyRole("USER", "ADMIN").antMatchers("/insertEtudiant").hasAnyRole("USER", "ADMIN")
+				.antMatchers("/user").hasAnyRole("USER", "ADMIN").antMatchers("/admin").hasRole("ADMIN").and()
+				.formLogin().loginPage("/login").permitAll().and().logout().permitAll();
+	}
+
+//	@Autowired
+//	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+//		auth.inMemoryAuthentication().withUser("admin").password("{noop}admin").roles("ADMIN").and().withUser("user")
+//				.password("{noop}user").roles("USER");
+//	}
+
+	@Autowired
+	public void configAuthentication(AuthenticationManagerBuilder auth) throws Exception {
+		auth.jdbcAuthentication().dataSource(dataSource).passwordEncoder(new BCryptPasswordEncoder(16))
+				.usersByUsernameQuery("select username,password, enabled from users where username=?")
+				.authoritiesByUsernameQuery("select username, role from user_roles where username=?");
 	}
 
 }
